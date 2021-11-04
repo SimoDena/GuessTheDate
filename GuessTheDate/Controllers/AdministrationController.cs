15dcd9b94db365d98dfae902dc07f38171e337aa
Simone Denardi <simone.denardi@lasportiva.com>
2021-11-04 09:30:30 +0100

    Added create role and list roles form.

---
+++ GuessTheDate/Controllers/AdministrationController.cs
@@ -0,0 +1,60 @@
+﻿using GuessTheDate.Models;
+using GuessTheDate.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GuessTheDate.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+
+        public AdministrationController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public ViewResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole()
+                {
+                    Name = model.RoleName
+                };
+
+                var result = await roleManager.CreateAsync(identityRole);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles", "Administration");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return View(model);
+        }
+
+        public ViewResult ListRoles()
+        {
+            var roles = roleManager.Roles;
+            return View(roles);
+        }
+    }
+}
